807a56c8bf12dabd2c3ccc68b5da69c63edaa160
dishant1123 <dishantshah1417@gmail.com>
2026-06-18 13:14:16 +0530

    intro_webscraping

---
+++ 18june.py
@@ -0,0 +1,34 @@
+"""
+web scrapping : website data  extract  , print  ,  save  . 
+
+HTML  CSS JS :  fronted ,backend 
+
+===============================================
+
+HTTP :  Hypertext Transfer Protocol : commnuication  between  brower and server. 
+
+brower 
+  | 
+HTTP request 
+  |
+server 
+  |
+HTTP response
+
+# get post put delete : 
+
+# lib for  web scraping : 
+requests 
+beautifulsoup
+selenium 
+playwright
+
+.html 
+
+<h1>STUDENTS </h1>
+<p>njenojwnjohhohhhwrhhiwr
+oknkorwngpjgpjp
+nwjwipjrpirwj </p>
+
+# hw : revise  HTML  tag  like  : p tag , h to h6 tag title tag  
+"""
