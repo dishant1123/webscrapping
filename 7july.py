a1521f2b795dd96dcd732fd5359cd5bca48dcbc6
dishant1123 <dishantshah1417@gmail.com>
2026-07-07 12:56:38 +0530

    ....

--- 7july.py
+++ 7july.py
@@ -1,6 +1,9 @@
 import selenium
 from selenium import webdriver
 import  time 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # ex :1 
 
 """driver = webdriver.Chrome()
@@ -10,7 +13,7 @@ driver.quit()
 """
 # ex:2 
 
-driver = webdriver.Chrome()
+"""driver = webdriver.Chrome()
 driver.get("https://quotes.toscrape.com/js/")
 
 # driver.get("https://www.amazon.com")
@@ -21,5 +24,25 @@ print(driver.title)
 print(driver.page_source[:500])
 
 driver.quit()
-
+"""
 # !pip install selenium
+
+#ex:3
+
+driver = webdriver.Chrome()
+driver.get("https://quotes.toscrape.com/js/")
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "quote"))
+)
+
+quotes = driver.find_elements(By.CLASS_NAME, "quote")
+
+for i in quotes:
+    text =i.find_element(By.CLASS_NAME, "text").text
+    author = i.find_element(By.CLASS_NAME, "author").text
+    print(text)
+    print(author)
+    print("-" * 40)
+    
+driver.quit()
