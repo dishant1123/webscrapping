349b59e67b6154f40e018cc7af850bf81c4d0a28
dishant1123 <dishantshah1417@gmail.com>
2026-07-09 12:40:57 +0530

    ...

---
+++ 9july.py
@@ -0,0 +1,38 @@
+import selenium
+from selenium import webdriver
+import  time 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+# ex :1  next button  : pagination  
+"""
+driver = webdriver.Chrome()
+driver.get("https://quotes.toscrape.com/js/")
+
+time.sleep(4)
+next_btn = driver.find_element(By.CSS_SELECTOR, "li.next a")
+next_btn.click()
+
+print(driver.title)
+print(driver.current_url)
+
+driver.quit()
+"""
+# ex : search  id :
+
+driver = webdriver.Chrome()
+driver.get("https://www.wikipedia.org")
+
+time.sleep(2)
+
+search = driver.find_element(By.ID, "searchInput")
+search.send_keys("Java programming")
+
+search.submit()
+
+time.sleep(3)
+print(driver.title)
+print(driver.current_url)
+print(driver.page_source[:500])
+driver.quit()
